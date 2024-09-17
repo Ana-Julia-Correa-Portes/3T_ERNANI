8f223bb4631a0d72843dec2c5144b7a8ff58090c
Correa <ana.julia.correa.portes@gmail.com>
2024-09-17 11:24:22 -0300

    loopWhile

---
+++ loopWhile/index.js
@@ -0,0 +1,15 @@
+// LOOOP WHILE (EQUANTO)
+// RODA NUM LOOP INFINITO POR PADRÃO
+// MAS PODEMOS ESTIPULAR UMA CONDIÇÃO
+// DE PARADA PARA CNTROLÁ-LO
+// SINTAXE : WHILE (CONDIÇÃO) {CÓDIGO}
+let n = 0
+while (n < 100) {
+   // console.clear();
+if (n % 2 == 1){
+  // SE O RESTO FOR 0 (É PAR)
+  // SE O RESTO FOR 1 (É ÍMPAR)
+  console.log(n);
+}
+n = n+1;
+  }
